15598299dab34bff0ccf32f82e494760ec700506
TheodoreUtomo <theodoreutomo@gmail.com>
2024-09-19 15:40:36 -0400

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Theodore Utomo on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordGardenTests/WordGardenTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenTests.swift
+//  WordGardenTests
+//
+//  Created by Theodore Utomo on 9/19/24.
+//
+
+import Testing
+@testable import WordGarden
+
+struct WordGardenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
